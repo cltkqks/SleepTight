001e33e8054c494e94afe57f7b5d47225f9227b7
han sol <mihcho1007@naver.com>
2017-08-18 07:56:33 +0000

    수정

--- opencv/main.py
+++ opencv/main.py
@@ -6,6 +6,7 @@ import cds
 import time
 import sys
 
+#조도센서로 불이 꺼지는것을 감지하면 사람의 움직임을 감지
 def humanDetection():
     cdsValue = 0
     cdsCount = 0
@@ -28,7 +29,9 @@ def humanDetection():
             else:
                 print('사람없음')
 
-
+#불이 꺼지고 사람이 움직이는 것이 감지되면 
+#사람이 잠드는 평균 시간인 30분간 모션 감지를 하여 
+#수면 감지 판별
 def sleepDetection():
     sleepdetect = 0
     totaldetect = 0
@@ -38,7 +41,7 @@ def sleepDetection():
     if sleepValue == True: #수면에 드는 시간은 보통 30분 이므로 30분동안 사람이 있는지 감지
         time.sleep(5) #정확한 분석을 위한 딜레이 //--60초
 
-        print('30분간 움직임 획수 측정 시작, !30초간 측정 시작')
+        print('30분간 움직임 획수 측정 시작, ?30초간 측정 시작')
         while count <= 3: #30분간 움직임 횟수 측정 //--30번
             print('일정 시간동안 모션 감지->')
             sleepdetect = detect.motionDetect(10)  #//--60초간 모션감지
@@ -51,18 +54,25 @@ def sleepDetection():
             else: #움직임이 없으면 exitcount +1
                 exitcount += 1
                 print('움직임 없음')
-
-            if totaldetect >= 1 and exitcount >= 0: #30분간 3번의 움직임이 감지되고 그 후 5분동안 연속으로 움직임이 없으면
-                print('수면 감지, 수면 패턴 측정 시작')
-                return True
-            elif totaldetect < 1: #30분간 1번 이하의 움직임이 감지되면 수면 감지 실패, 잠자리에 사람이 없음
-                print('수면 감지 실패, 사람 없음')
+            light = cds.light(4)
+            if light < 1000:
+                print('light on, 수면감지 실패')
                 return False
 
+        if totaldetect >= 1 and exitcount >= 0: #30분간 3번의 움직임이 감지되고 그 후 5분동안 연속으로 움직임이 없으면
+            print('수면 감지, 잠자는 사람 존재')
+            return True
+        elif totaldetect < 1: #30분간 1번 이하의 움직임이 감지되면 수면 감지 실패, 잠자리에 사람이 없음
+            print('수면 감지 실패, 사람 없음')
+            return False
+
+
+#수면 패턴 측정
 def sleepPattern():
     sleepValue = sleepDetection() #수면 감지
     if sleepValue == True:
         print('수면 패턴 측정 시작')
+        
     elif sleepValue == False:
         print('수면 감지 재시작')
         sleepPattern()
