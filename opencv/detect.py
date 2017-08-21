79ddcb3882e7f80626e7b8facd21434d5c973586
han sol <mihcho1007@naver.com>
2017-08-21 08:45:25 +0000

    motiondetect 함수 인자값 추가

--- opencv/detect.py
+++ opencv/detect.py
@@ -73,7 +73,8 @@ def readFile():
 #모션 감지 함수
 #모션 감지 수행하는 시간을 time을 통해 조절할수 있고 
 #모션감지함수를 시간제한없이 동작시키려면 time에 0을 넣어준다    
-def motionDetect(time):
+#함수 인자 : time - 모션 감지 수행 시간, contourValue - 모션감지 민감도 설정
+def motionDetect(time, contourValue):
     global frame, frame2, inputmode, fps
     count = 0 #감지시간을 위한 카운터 값
     number = 0 #일정 시간내 모션 감지 숫자 카운트 
@@ -83,6 +84,10 @@ def motionDetect(time):
     cv2.namedWindow('frame')
     cv2.setMouseCallback('frame', onMouse, param=(frame,frame2))
         
+    if contourValue == 100000:
+        print('감지구역 재설정을 위해 i를 누르고 마우스를 이용하여 구역을 재설정 하세요.')
+        print('감지 구역 재설정을 끝내려면 esc 키를 누르세요.')
+
 
     while True:
         ret, frame = cap.read()
@@ -94,25 +99,32 @@ def motionDetect(time):
         _, contours, _ = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # contours를 찾는다
         count += 1
         for c in contours: # 한 프레임의 contour들을 모두 찾아 검사해 움직임을 감지
-            if cv2.contourArea(c) < 10000 or count*fps < 2*fps:#범위: #일정한 크기이상의 contours가 없으면 아래는 무시하고 while루프를 돈다
+            areaValue = cv2.contourArea(c)
+            if areaValue < contourValue or areaValue > 100000 or count*fps < 2*fps:
+            #범위: #일정한 크기이상의 contours가 없으면 아래는 무시하고 while루프를 돈다
                 continue
             elif time != 0 : #일정시간내 감지 동작시 
                 print('MD')
+                print('픽셀 크기: %d' % areaValue)
                 number += 1 #모션 감지 카운트
             elif time == 0:
                 print('MD')
+                print('픽셀 크기: %d' % areaValue)
                 number += 1
-                if number >= 2:
-                    return True #시간제한 없이 감지 동작-감지 동작하면 리턴
+                return areaValue #수면 기록 테스트를 위한 리턴값
+                #return True #시간제한 없이 감지 동작-감지 동작하면 리턴
+            elif time == -1:
+                print('MD')
+                print('픽셀 크기: %d' % areaValue)
                 
         cv2.rectangle(frame,(col,row),(col+width,row+height),(0,255,0),2)
         cv2.imshow('frame', frame)
         k=cv2.waitKey(1)&0xFF
 
         if k==27: #ess 키로 강제 종료시
-            print('강제 종료')
+            print('esc키로 종료')
             #cap.release()
-            #cv2.destroyAllWindows()
+            cv2.destroyAllWindows()
             return False
 
         if k==ord('i'): #'i' 키로 감지 영역 재설정
@@ -124,7 +136,7 @@ def motionDetect(time):
                 cv2.imshow('frame',frame)
                 cv2.waitKey(0)
 
-        if time != 0 and count >= fps*time: #모션감지 동작시간 time값이 0이면 계속 동작
+        if time != 0 and count >= fps*time and time != -1: #모션감지 동작시간 time값이 0이면 계속 동작
             return number #일정시간 동안 움직임 감지하고 감지 횟수값 리턴
 
     #cap.release()
