4ee9036bf33bb66863bd8a9c1320e8ce1b434143
han sol <mihcho1007@naver.com>
2017-08-24 15:24:07 +0000

    완료

--- opencv/cds.py
+++ opencv/cds.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 import time
 
 
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
 
 """
 라즈베리파이는 아날로그 값을 읽지 못해 조도센서를 그대로 사용할수 없다
@@ -16,6 +16,7 @@ GPIO.setmode(GPIO.BCM)
 #빛이 있으면 300, 어두워지면 1000 이상을 리턴
 #+:1 -:6(그라운드) GPIO핀:7(cds와 커페시터 사이)
 def light(pin):
+    GPIO.setmode(GPIO.BCM)
     count = 0
 
     #Output
@@ -35,3 +36,27 @@ def light(pin):
 #GPIO 초기화
 def clean():
     GPIO.cleanup()
+
+
+def irLedon(pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+
+def irLedoff(pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+
+#print('on')
+#irLedon(26)
+#time.sleep(3)
+#print('off')
+#irLedoff(26)
+#time.sleep(3)
+#print('on')
+#irLedon(26)
+#time.sleep(3)
+#print('off')
+#irLedoff(26)
+#clean()
