13d66a3e03643c4344564cda5becffa2e2517a45
han sol <mihcho1007@naver.com>
2017-09-07 11:33:03 +0000

    opencv를 이용한 스트리밍 방식으로 수정 및 서버 조작 함수 추가

--- stream/camera.py
+++ stream/camera.py
@@ -3,8 +3,9 @@ import cv2
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
-
-
+        self.video.set(3,640) #width 
+        self.video.set(4,480) #heigth 
+        self.video.set(cv2.CAP_PROP_FPS, 5)
 
     
     def __del__(self):
