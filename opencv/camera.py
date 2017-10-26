cb4f3d21742a80541e5f18c83412277378506154
han sol <mihcho1007@naver.com>
2017-10-26 12:52:03 +0000

    x

--- opencv/camera.py
+++ opencv/camera.py
@@ -5,7 +5,7 @@ class VideoCamera(object):
         self.video = cv2.VideoCapture(0)
         self.video.set(3,640) #width 
         self.video.set(4,480) #heigth 
-        self.video.set(cv2.CAP_PROP_FPS, 5)
+        self.video.set(cv2.CAP_PROP_FPS, 3)
 
     
     def __del__(self):
